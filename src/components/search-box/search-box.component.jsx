0e6436d4eda8fa8ff557da67769dc74e9dffe1bf
unknown <raphesezobor@gmail.com>
2021-12-11 03:05:15 -0800

    update styling

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,11 +3,8 @@ import './search-box.styles.css';
 
 export const SearchBox = ({ placeholder, handleChange }) => {
     return (
-        <div className="searchBox">
             <input className="searchBox" type="search" placeholder={placeholder} 
     onChange = {handleChange}
     />
-        </div>
-    
     )
 };
